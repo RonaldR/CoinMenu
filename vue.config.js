4eb5c2f2d1eee10887d0f609b15a94ad43da2ed1
Ronald Runia <rrunia@gmail.com>
2019-02-23 19:49:12 +0100

    pwa settings

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,33 @@
 module.exports = {
     lintOnSave: true,
     publicPath: '',
+    pwa: {
+        name: 'CoinMenu',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'black',
+        workboxOptions: {
+            runtimeCaching: [{
+                urlPattern: new RegExp('^https://cors-anywhere.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?convert=USD&limit=5000'),
+                handler: 'networkFirst',
+                    options: {
+                    networkTimeoutSeconds: 20,
+                    cacheName: 'coin-data',
+                    cacheableResponse: {
+                        statuses: [0, 200],
+                    },
+                },
+            },
+            {
+                urlPattern: new RegExp('^https://cors-anywhere.herokuapp.com/https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/latest?convert=USD'),
+                handler: 'networkFirst',
+                    options: {
+                    networkTimeoutSeconds: 20,
+                    cacheName: 'cap-data',
+                    cacheableResponse: {
+                        statuses: [0, 200],
+                    },
+                },
+            }]
+        }
+    }
 };
