37ba314b0226386a0f10b0935b81388da10e5cd6
Ronald Runia <rrunia@gmail.com>
2019-02-24 10:07:42 +0100

    PWA regex fix

--- vue.config.js
+++ vue.config.js
@@ -6,28 +6,30 @@ module.exports = {
         appleMobileWebAppCapable: 'yes',
         appleMobileWebAppStatusBarStyle: 'black',
         workboxOptions: {
-            runtimeCaching: [{
-                urlPattern: new RegExp(`^https:\/\/cors-anywhere\.herokuapp\.com\/https:\/\/pro-api\.coinmarketcap\.com\/v1\/cryptocurrency\/listings\/latest\?convert=USD.*`),
-                handler: 'networkFirst',
-                options: {
-                    networkTimeoutSeconds: 10,
-                    cacheName: 'listingsdata',
-                    cacheableResponse: {
-                        statuses: [0, 200],
+            runtimeCaching: [
+                {
+                    urlPattern: new RegExp('^https:\/\/cors-anywhere\.herokuapp\.com.*'),
+                    handler: 'networkFirst',
+                    options: {
+                        networkTimeoutSeconds: 10,
+                        cacheName: 'listingsdata',
+                        cacheableResponse: {
+                            statuses: [0, 200],
+                        },
                     },
                 },
-            },
-            {
-                urlPattern: new RegExp(`^https:\/\/cors-anywhere\.herokuapp\.com\/https:\/\/pro-api\.coinmarketcap\.com\/v1\/global-metrics\/quotes\/latest\?convert\=USD`),
-                handler: 'networkFirst',
-                options: {
-                    networkTimeoutSeconds: 10,
-                    cacheName: 'quotesdata',
-                    cacheableResponse: {
-                        statuses: [0, 200],
-                    },
-                },
-            }]
-        }
-    }
+                // {
+                //     urlPattern: new RegExp(`^https:\/\/cors-anywhere\.herokuapp\.com\/https:\/\/pro-api\.coinmarketcap\.com\/v1\/global-metrics\/quotes\/latest\?convert\=USD`),
+                //     handler: 'networkFirst',
+                //     options: {
+                //         networkTimeoutSeconds: 10,
+                //         cacheName: 'quotesdata',
+                //         cacheableResponse: {
+                //             statuses: [0, 200],
+                //         },
+                //     },
+                // }
+            ],
+        },
+    },
 };
